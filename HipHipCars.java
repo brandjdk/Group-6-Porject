f0993eec413aae1cee85d31cae5d2d7ddec926d7
brandjdk <38508816+brandjdk@users.noreply.github.com>
2018-04-22 12:46:58 -0400

    Add files via upload

--- HipHipCars.java
+++ HipHipCars.java
@@ -2,4 +2,20 @@ public class HipHipCars{
    public static void main(String args[]){
       
    }
+   
+   public static void AddCustomer(){
+      
+   }
+   
+   public static void RemoveCustomer(){
+      
+   }
+   
+   public static void AccessCustomer(){
+      
+   }
+   
+   public static void Terminate(){
+      System.exit(0);
+   }
 }
